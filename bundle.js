14f2f687f975912a6a09c588e398d3d67bab77a0
Arjun <arjundutta91@gmail.com>
2016-03-08 16:15:37 -0800

    add basic game function - player control, enemy spawn, point display

--- bundle.js
+++ bundle.js
@@ -70,16 +70,21 @@
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	
+	var Util = __webpack_require__(4);
+
 	var PlayerCell = __webpack_require__(2);
+	var RivalCell = __webpack_require__(7);
 	var EnemyCell = __webpack_require__(5);
 
-	var DIM_X = window.innerWidth;
-	var DIM_Y = window.innerHeight;
+	DIM_X = window.innerWidth;
+	DIM_Y = window.innerHeight;
 
 	var Game = function () {
 	  this.enemyCells = [];
 	  this.playerCell = {};
+	  this.rivalCell = {};
+	  this.points = 0;
+	  this.gameOver = false;
 	};
 
 	Game.prototype.add = function (object) {
@@ -87,6 +92,8 @@
 	    this.playerCell = object;
 	  } else if (object instanceof EnemyCell) {
 	    this.enemyCells.push(object);
+	  } else if (object instanceof RivalCell) {
+	    this.rivalCell = object;
 	  } else {
 	    throw 'hell';
 	  }
@@ -94,13 +101,16 @@
 
 	Game.prototype.addPlayerCell = function() {
 	  var playerCell = new PlayerCell({
-	    pos: [DIM_X/2, DIM_Y/2]
+	    pos: [100, DIM_Y/2]
 	  });
 	  this.add(playerCell);
+	};
 
-	  this.add(createEnemyCell());
-
-	  return playerCell;
+	Game.prototype.addRivalCell = function() {
+	  var rivalCell = new RivalCell({
+	    pos: [DIM_X - 100, DIM_Y/2]
+	  });
+	  this.add(rivalCell);
 	};
 
 	Game.prototype.draw = function(ctx) {
@@ -108,12 +118,64 @@
 	  ctx.fillStyle = "#000000";
 	  ctx.fillRect(0, 0, DIM_X, DIM_Y);
 
+	  ctx.font = "30px Arial";
+	  ctx.fillStyle = "red";
+	  ctx.fillText(this.points,10,50);
+
 	  this.playerCell.vel = this.findVel(this.playerCell.pos);
 	  this.playerCell.move();
+
 	  this.playerCell.draw(ctx);
+	  this.rivalCell.draw(ctx);
+
+	  this.checkCollisions();
+	  this.checkEnemies();
+	  this.moveEnemies();
 	  this.enemyCells.forEach(function(cell) {
 	    cell.draw(ctx);
 	  });
+	};
+
+	Game.prototype.moveEnemies = function() {
+	  this.enemyCells.forEach(function(cell) {
+	    cell.move();
+	  });
+	};
+
+	Game.prototype.checkCollisions = function() {
+	  var player = this.playerCell
+	  this.enemyCells.forEach(function(cell) {
+	    if (cell.hasCollidedWith(player)) {
+	      this.handleCollision(player, cell);
+	    }
+	  }.bind(this));
+	};
+
+	Game.prototype.handleCollision = function(player, enemy) {
+	  if (player.radius > enemy.radius) {
+	    this.consume(player, enemy);
+	  } else if (enemy.radius >= player.radius) {
+	    // this.gameOver = true;
+	    alert('GAME OVER');
+	  }
+	};
+
+	Game.prototype.checkEnemies = function() {
+	  var numEnemies = Math.floor(this.playerCell.radius / 5)
+	  if (numEnemies < 5) { numEnemies = 5 };
+	  if (numEnemies > 15) { numEnemies = 10 };
+	  while (this.enemyCells.length < numEnemies) {
+	    this.add(this.createEnemyCell());
+	  }
+	};
+
+	Game.prototype.consume = function(player, enemy) {
+	  var growth = (enemy.radius / 4);
+	  var idx = this.enemyCells.indexOf(enemy);
+	  this.enemyCells.splice(idx, 1);
+	  player.radius += growth;
+	  this.points += Math.floor(growth);
+	  console.log(this.points);
 	};
 
 	Game.prototype.findVel = function(pos, that) {
@@ -127,12 +189,23 @@
 	  return [(diffX), (diffY)];
 	};
 
-	var createEnemyCell = function() {
+	Game.prototype.createEnemyCell = function() {
+	  var pos = Util.randomPos();
+	  var radius = Util.randomSize(this.playerCell.radius);
+	  var vel = Util.findVel(radius);
+
 	  var enemyCell = new EnemyCell({
-	    pos: [150, 150],
-	    vel: [0, 0]
+	    pos: pos,
+	    vel: vel,
+	    radius: radius
 	  });
 	  return enemyCell
+	};
+
+	Game.prototype.clear = function(ctx) {
+	  ctx.clearRect(0, 0, DIM_X, DIM_Y);
+	  ctx.fillStyle = "#000000";
+	  ctx.fillRect(0, 0, DIM_X, DIM_Y);
 	};
 
 	module.exports = Game;
@@ -154,16 +227,6 @@
 	  MovingObject.call(this, options);
 	}
 
-	PlayerCell.drawTest = function(ctx) {
-	  ctx.fillStyle = this.color;
-
-	  ctx.beginPath();
-	  ctx.arc(
-	    this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
-	  );
-	  ctx.fill();
-	};
-
 	Util.inherits(PlayerCell, MovingObject);
 
 	module.exports = PlayerCell;
@@ -171,9 +234,10 @@
 
 /***/ },
 /* 3 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	
+	var Util = __webpack_require__(4);
+
 	var MovingObject = function (options) {
 	  this.pos = options.pos;
 	  this.vel = options.vel;
@@ -192,12 +256,29 @@
 	};
 
 	MovingObject.prototype.move = function() {
-	  this.pos[0] += this.vel[0];
-	  this.pos[1] += this.vel[1];
+	  var newPos = [];
+	  newPos[0] = this.pos[0] += this.vel[0];
+	  newPos[1] = this.pos[1] += this.vel[1];
+
+	  if (newPos[0] > DIM_X || newPos[0] < 0) {
+	    this.vel[0] = this.vel[0] * -1
+	    newPos[0] = this.pos[0] += this.vel[0];
+	  } else if (newPos[1] > DIM_Y || newPos[1] < 0) {
+	    this.vel[1] = this.vel[1] * -1
+	    newPos[1] = this.pos[1] += this.vel[1];
+	  }
+
+	  this.pos = newPos;
 	};
 
-	MovingObject.prototype.collideWith = function(otherObject) {
+	MovingObject.prototype.hasCollidedWith = function(otherObject) {
+	  var distance = Util.distance(this.pos, otherObject.pos);
 
+	  if (distance < this.radius || distance < otherObject.radius) {
+	    return true;
+	  } else {
+	    return false;
+	  }
 	};
 
 
@@ -211,6 +292,13 @@
 	
 	var Util = function() { };
 
+	var VEL_TRANSFORMS = {
+	  1: [1, 1],
+	  2: [1, -1],
+	  3: [-1, 1],
+	  4: [-1, -1]
+	};
+
 	Util.inherits = function(child, base) {
 	  function Surrogate () { this.constructor = child };
 	  Surrogate.prototype = base.prototype;
@@ -221,9 +309,28 @@
 	  var distX = pos1[0] - pos2[0];
 	  var distY = pos1[1] - pos2[1];
 	  var distance = Math.sqrt((distX * distX) + (distY * distY));
-	  return [distX, distY];
+	  return distance;
 	};
-	window.Util = Util;
+
+	Util.randomPos = function() {
+	  return [getRandomInRange(0, DIM_X), getRandomInRange(0, DIM_Y)];
+	};
+
+	Util.randomSize = function(playerRadius) {
+	  var size = getRandomInRange(5, (playerRadius + 25));
+	  return size;
+	};
+
+	Util.findVel = function(radius) {
+	  var vector = VEL_TRANSFORMS[getRandomInRange(1, 4)];
+	  var initial = [(vector[0] * Math.random())*10, (vector[1] * Math.random())*10];
+	  return [initial[0]/(radius/6), initial[1]/(radius/6)];
+	};
+
+	var getRandomInRange = function(min, max) {
+	  return (Math.floor(Math.random() * (max - min + 1)) + min);
+	};
+
 	module.exports = Util;
 
 
@@ -237,21 +344,12 @@
 	var EnemyCell = function(options) {
 	  options.pos = options.pos;
 	  options.vel = options.vel || [0, 0];
-	  options.radius = 5;
+	  options.radius = options.radius;
 	  options.color = "#00F";
 
 	  MovingObject.call(this, options);
 	};
 
-	EnemyCell.prototype.drawTest = function(ctx) {
-	  ctx.fillStyle = this.color;
-
-	  ctx.beginPath();
-	  ctx.arc(
-	    this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
-	  );
-	  ctx.fill();
-	};
 
 	Util.inherits(EnemyCell, MovingObject);
 
@@ -274,6 +372,7 @@
 	  this.game = game;
 	  this.ctx = ctx;
 	  this.game.addPlayerCell();
+	  this.game.addRivalCell();
 	};
 
 	GameView.prototype.start = function() {
@@ -287,6 +386,27 @@
 
 
 	module.exports = GameView;
+
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var MovingObject = __webpack_require__(3);
+	var Util = __webpack_require__(4);
+
+	var RivalCell = function(options) {
+	  options.pos = options.pos;
+	  options.vel = options.vel || [0, 0];
+	  options.radius = 15;
+	  options.color = "#F00";
+
+	  MovingObject.call(this, options);
+	}
+
+	Util.inherits(RivalCell, MovingObject);
+
+	module.exports = RivalCell;
 
 
 /***/ }
